ee808ecea8467143c3c03b6185f86c1340481180
Jingfei Chang <68282985+JingfeiChang@users.noreply.github.com>
2020-09-28 20:07:53 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -14,3 +14,5 @@ from .se_preresnet import *
 from .se_inception import *
 from .vgg import *
 from .se_vgg import *
+from .imagenet_resnetp import resnetp18, resnetp34, resnetp50, resnetp101, resnetp152
+from .imagenet_seresnet import seresnet18, seresnet34, seresnet50, seresnet101, seresnet152
